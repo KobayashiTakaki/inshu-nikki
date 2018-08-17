13c2eaebfe0ff87b089fe2c55c0f7949ea498e9c
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-08-17 12:38:48 +0900

    fix HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,32 +35,15 @@ class HomeController extends Controller
          $user_id = $session_data['user_id'];
        }
        */
-       
+
        //$inshus = [];
 
-       $inshus = Inshu::where('user_id', Auth::id())
-                 ->get();
-       //var_dump($kinds);
-       /*
-       foreach ($kinds as $kind) {
-          $amount = Inshu::where('user_id', $user_id)
-                     ->where('kind', $kind)
-                     ->sum('amount');
-         $inshus[$kind] = $amount;
-       }
-       */
-       $inshu_sum =[];
-       $kinds = $inshus->unique('kind')->pluck('kind');
-       foreach ($kinds as $kind) {
-         $amount_sum = $inshus->where('kind', $kind)->sum('amount');
-         array_push($inshu_sum, ['kind' => $kind, 'amount' => $amount_sum]);
-       }
 
        //var_dump($inshu_sum);
 
 
 
-       return view('home', compact('username','user_id', 'inshu_sum'));
+       return view('home');
      }
 
 
