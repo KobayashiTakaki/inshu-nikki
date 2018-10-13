98212b50829cfe2a079d5ee0e52acf3908c1ef44
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-10-14 00:15:46 +0900

    mod route

--- app/Http/Controllers/InshuController.php
+++ app/Http/Controllers/InshuController.php
@@ -43,33 +43,11 @@ class InshuController extends Controller
       $dateTo = $request->dateTo;
       $inshus = Inshu::where('user_id', Auth::id())
                 ->whereBetween('date', [$dateFrom, $dateTo])
-                ->orderBy('date')
+                ->orderBy('date', 'desc')
                 ->get();
       return $inshus;
     } catch (Exception $e) {
       return $e->getMessage();
-    }
-  }
-  
-  public function apiTest(Request $request)
-  {
-    $type = $request->type;
-    switch($type)
-    {
-      case 'A':
-        return [
-          ['id'=> 1, 'date'=> '2018/09/01', 'kind' => 'あ', 'amount'=> 100],
-          ['id'=> 2, 'date'=> '2018/09/02', 'kind' => 'い', 'amount'=> 110],
-          ['id'=> 3, 'date'=> '2018/09/03', 'kind' => 'う', 'amount'=> 120],
-        ];
-        break;
-      case 'B':
-        return [
-          ['id'=> 4, 'date'=> '2018/10/01', 'kind' => 'え', 'amount'=> 200],
-          ['id'=> 5, 'date'=> '2018/10/02', 'kind' => 'お', 'amount'=> 210],
-        ];
-        break;
-      return 'invalid param.';
     }
   }
 
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function(){
-    return view('home');
-  });
+Route::get('/', function(){ return view('home'); });
 /*
 Route::get('register', function(){
     return view('register');
