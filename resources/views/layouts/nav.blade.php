e27317b4c4facca4c32c95aabfe3c4cb346dfa93
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-10-14 00:23:16 +0900

    mod nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,7 +11,7 @@
           <span class="nav-link">{{ Auth::user()->name }}</span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/logout">
+          <a class="nav-link" href="logout">
             {{ __('Logout') }}
           </a>
         </li>
